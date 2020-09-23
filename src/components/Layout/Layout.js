bfe2ac6266ccfa3a8d156b54e19a22033de9c916
srinu6 <46079085+srinu6@users.noreply.github.com>
2020-09-24 00:58:40 +0530

    Save before ejecting

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Layout.css';
+
+const layout =(props) =>(
+ 
+    <div className='Content'>Toolbar, sidedrawer, Backdrop</div>
+    
+ 
+)
+export default layout;
