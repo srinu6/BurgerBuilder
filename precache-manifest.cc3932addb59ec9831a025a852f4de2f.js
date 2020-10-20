91a39750343a87d6b3982cca72e2d091c155acd0
srinu6 <46079085+srinu6@users.noreply.github.com>
2020-10-20 10:31:10 +0530

    Updates

---
+++ precache-manifest.cc3932addb59ec9831a025a852f4de2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b310d060952594397437be40dd3aab",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "f9d56977fbeade29e26b",
+    "url": "/BurgerBuilder/static/css/main.1e5493e7.chunk.css"
+  },
+  {
+    "revision": "bb5681f20862d1f619fc",
+    "url": "/BurgerBuilder/static/js/2.24a2bb00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerBuilder/static/js/2.24a2bb00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d56977fbeade29e26b",
+    "url": "/BurgerBuilder/static/js/main.15201b90.chunk.js"
+  },
+  {
+    "revision": "ea32caca645dacc3ee86",
+    "url": "/BurgerBuilder/static/js/runtime-main.99044ad6.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
